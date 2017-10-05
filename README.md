ad1d1c71db7798d5e1a2768ec6a15e9d0bebbebf
AdamSkuse <a.t.skuse@gmail.com>
2017-10-05 14:43:58 -0400

    Initialises repo. Static site index and styles copied.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Wordpressification of a static site
+
+Takes one of my static webpages and converts it into a wordpress theme.
+
+## Objectives
+**1** Take static site HTML, CSS and JS and put it into Wordpress theme
+**2** Add PHP code to allow blog posts and events schedule
+**3** Make it into a reusable Wordpress theme
---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>AdamSkuse ... com</title>
+  <meta name="AdamSkuse.com" content="AdamSkuse.com">
+  <meta name="author" content="Adam">
+
+  <link href="<?php bloginfo( 'stylesheet_url' ); ?>" rel="stylesheet" media="all" type="text/css" />
+
+  <link href="https://fonts.googleapis.com/css?family=IM+Fell+Great+Primer" rel="stylesheet">
+  <link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
+  <script src="https://use.fontawesome.com/5cfe35cd23.js"></script>
+</head>
+
+<body>
+    <div class="fixed-header">
+        <div class="fixed-header__title">
+            <h1>Adam Skuse ... com</h1>
+            <p>The official real 100% actual website of Adam Skuse, standupper and podcasterteer</p>
+        </div>
+    </div>
+
+    <div class="main-section">
+        <div class="main-section__image">
+         <!-- <img src="assets/images/pilot.jpg" alt="pilot" class="main-image" width="650px"></img> -->
+            <img alt="" src="<?php bloginfo( 'template_url' ); ?>/images/pilot.jpg" class="main-image" width="650px"></img>
+        </div>
+
+        <div class="panel-wrapper">
+            <div class="panel about-panel">
+              <h3>About</h3>
+              <p>UK-born me first took to the stage in a purposefully comedic capacity in 2013 while living in Shanghai, China. Within 6 months of starting out, he'd <a href="http://www.tavistock-today.co.uk/article.cfm?id=4227&headline=Comedian%20Adam%20stands%20up%20and%20gets%20heard%20in%20competition&sectionIs=news&searchyear=2013">won</a> the AudioGo Best New UK Comic Award.</p><br>
+
+              <p>Since then, he (me) has performed across Asia and the UK as a feature act and headliner. In 2016 he took his first hour to the Edinburgh Fringe Festival.</p><br>
+
+              <p>Adam (I) recently moved to Los Angeles, where he also writes, podcasts and assimilates into the local culture.</p>
+
+
+            </div>
+            <div class="panel video-panel">
+                <h3>Videos</h3>
+                  <div class="video-panel__video-wrapper">
+                    <h4>Cats on planes, dogs in clothing</h4>
+                    <iframe width="374" height="210" src="https://www.youtube.com/embed/sR5rb3o1GDg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                  </div>
+                  <div class="video-wrapper">
+                    <h4>Achille's heel</h4>
+                    <iframe width="374" height="210" src="https://www.youtube.com/embed/WYIBeEsOgUI?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                  </div>
+                  <div class="video-wrapper">
+                    <h4>Entente cordiale</h4>
+                    <iframe width="374" height="210" src="https://www.youtube.com/embed/9lEnm0HDaxQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                  </div>
+            </div>
+            <div class="panel podcasts-panel">
+              <h3>Podcasts</h3>
+                <div class="podcast__logo">
+                  <a href="http://www.bestlaidplanspodcast.podbean.com/" target="_blank"><img src="assets/images/podcast-1.jpg" alt="Best Laid Plans logo"></a>
+                </div>
+                <div class="podcast__description">
+                  <h4>Best Laid Plans</h4>
+                  <p>In which I chat with entrepreneurs about their experiences setting up and running their businesses.</p>
+                </div>
+                <div class="podcast__logo">
+                  <a href="http://www.polymorphself.com/" target="_blank"><img src="assets/images/podcast-2.jpg" alt="Polymorphself logo"></a>
+                </div>
+                <div class="podcast__description">
+                  <h4>Polymorphself</h4>
+                  <p>I played a magical elf in this Dungeons &amp; Dragons podcast until I was sucked hard into the void in episode 32. Spoiler.</p>
+                </div>
+                <div class="podcast__logo">
+                  <a href="http://kurtdunsing.squarespace.com/ltaboutg/" target="_blank"><img src="assets/images/podcast-3.jpg" alt="LTAG logo"></a>
+                </div>
+                <div class="podcast__description">
+                  <h4>Let's Talk About Greg</h4>
+                  <p>I occassionally appear on this hilarious podcast hosted by funnyman Kurt Dunsing.</p>
+                </div>
+            </div>
+            <div class="panel webcam-panel">
+              <h3>Nude Webcam</h3>
+              <p>Error 452: proxy-disconnect /// server-overload.</p>
+              <p>Please try again later when traffic volume has diminished.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="fixed-footer">
+        <nav><ul><li id="home">Home</li><li id="about">About</li><li id="video">Video</li><li id="podcasts">Podcasts</li><li id="webcam">Nude-webcam</li></ul></nav>
+        <div class="social-icons">
+          <a href="https://www.youtube.com/user/MonadicFlaccidity/" target="_blank"><i class="fa fa-youtube fa-lg" aria-hidden="true"></i></a><a href="https://twitter.com/AdamSkuse" target="_blank"><i class="fa fa-twitter-square fa-lg" aria-hidden="true"></i></a>
+        </div>
+    </div>
+
+    <script src="assets/scripts/App-5ceb708257.js"></script>
+</body>
+
+</html>
+
